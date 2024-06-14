6c0e561e97443b8c1969967228afe9a203d0c99c
Md Fuad Amir <mdfuadamir@gmail.com>
2024-06-14 17:32:58 +0600

    added head section

---
+++ src/Components/Head/Head.jsx
@@ -0,0 +1,35 @@
+import { FaFacebookF } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Head = () => {
+    return (
+        <div className="px-6 md:px-16 py-4">
+            <div className="flex justify-between items-center">
+                {/* left head */}
+                <div className="">
+                    <h2 className="font-bold text-2xl md:text-4xl font-serif">EDUCATION</h2>
+                    <p className="font-serif">ONLINE EDUCATION & LEARNING</p>
+                </div>
+                {/* right head */}
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+                    <Link><FaFacebookF size={35} className="rounded-full p-2 bg-slate-100 
+                    text-[#1eb2a6] hover:bg-[#1eb2a6] hover:text-white duration-200"/></Link>
+                    <Link>
+                    <FaInstagram size={35} className="rounded-full p-2 bg-slate-100 text-[#1eb2a6] hover:bg-[#1eb2a6] hover:text-white duration-200"/>
+                    </Link>
+                    <Link>
+                    <FaTwitter size={35} className="rounded-full p-2 bg-slate-100 text-[#1eb2a6] hover:bg-[#1eb2a6] hover:text-white duration-200"/>
+                    </Link>
+                    <Link>
+                    <FaYoutube size={35} className="rounded-full p-2 bg-slate-100 text-[#1eb2a6] hover:bg-[#1eb2a6] hover:text-white duration-200"/>
+                    </Link>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+export default Head;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,31 @@
+import { Link } from "react-router-dom";
+import Head from "../Head/Head";
 
 
 const Header = () => {
     return (
-        <nav>
+        <>
+        <Head/>
+        <nav className="px-4 md:px-12">
+            <div className="flex justify-between items-center">
+            {/* links */}
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/courses">All Courses</Link>
+                <Link to="/about">About</Link>
+                <Link to="/team">Team</Link>
+                <Link to="/pricing">Pricing</Link>
+                <Link to="/journal">Journal</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            {/* start */}
+            <div>
+               <h1>GET CERTIFICATE</h1> 
+            </div>
+            </div>
             
         </nav>
+        </>
     );
 };
 
