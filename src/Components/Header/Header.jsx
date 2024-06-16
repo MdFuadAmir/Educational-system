4ad8f73fd9c5019690d867eedefe3507e48bd8e3
Md Fuad Amir <mdfuadamir@gmail.com>
2024-06-17 00:28:32 +0600

    added navber section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,21 +6,21 @@ const Header = () => {
     return (
         <>
         <Head/>
-        <nav className="px-4 md:px-12">
-            <div className="flex justify-between items-center">
+        <nav className="mx-4 text-white">
+            <div className=" md:flex justify-between items-center bg-slate-100 bg-opacity-20">
             {/* links */}
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/courses">All Courses</Link>
-                <Link to="/about">About</Link>
-                <Link to="/team">Team</Link>
-                <Link to="/pricing">Pricing</Link>
-                <Link to="/journal">Journal</Link>
-                <Link to="/contact">Contact</Link>
+            <div className="flex flex-col text-center md:flex-row gap-6 text-md font-bold px-6 py-8">
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/">Home</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/courses">All Courses</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/about">About</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/team">Team</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/pricing">Pricing</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/journal">Journal</Link>
+                <Link className="hover:text-[#1eb2a6] duration-200" to="/contact">Contact</Link>
             </div>
             {/* start */}
-            <div>
-               <h1>GET CERTIFICATE</h1> 
+            <div className="bg-[#1eb2a6] py-8 px-12 text-center items-center">
+                <Link to="/">GET CERTIFICATE</Link>
             </div>
             </div>
             
